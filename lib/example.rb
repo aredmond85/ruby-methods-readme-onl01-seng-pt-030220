dba7b978bfd8fcb582a287d0bc7d5ea3f5e979c5
Austin Redmond <aredmond85@gmail.com>
2020-03-02 15:46:11 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,6 +1,17 @@
-# Use this space to code along with the readme
-
 phrase = "Hello World!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+
+# ... The rest of the program
+
 puts phrase
 puts phrase
 puts phrase
